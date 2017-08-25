4a2c36c06cd2da60b5a1a126664ee78ce571cb1c
Christoph Porwol <chporwol@gmail.com>
2017-08-25 19:45:07 -0400

    Added listings, search and details

--- listingController.js
+++ listingController.js
@@ -1,14 +1,17 @@
 angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
   function($scope, Listings) {
     $scope.listings = Listings;
-    $scope.detailedInfo = undefined;
+    $scope.detailedInfo = $scope.listings[0];
 
     /* 
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
+    $scope.addListing = function() {}
     $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+
+    $scope.showDetails = function(index) {
+      $scope.detailedInfo = $scope.listings[index];
+    };
   }
 ]);
