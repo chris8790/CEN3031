6b0de492980a855c6da19b560cd710ede6bfaa6b
Christoph Porwol <chporwol@gmail.com>
2017-08-29 18:28:45 -0400

    Assignment with CSS

--- listingController.js
+++ listingController.js
@@ -2,13 +2,21 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = $scope.listings[0];
+    $scope.newEntry = undefined;
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
-     */
-    $scope.addListing = function() {}
-    $scope.deleteListing = function(index) {};
+    $scope.addListing = function() {
+      $scope.newEntry.code = $scope.newEntry.code.toUpperCase();
+      $scope.listings.push($scope.newEntry);
+      $scope.newEntry = undefined;
+    };
+
+    $scope.addListingReset = function() {
+      $scope.newEntry = undefined;
+    }
+    
+    $scope.deleteListing = function(index) {
+      $scope.listings.splice(index, 1);
+    };
 
     $scope.showDetails = function(index) {
       $scope.detailedInfo = $scope.listings[index];
